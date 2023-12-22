dc2e8a92087d1425a63fff21a77f164987f556c4
Christian Deese <s238007@students.aie.edu>
2023-12-22 10:24:02 -0600

    added Waypoint class and contructor but is WIP

---
+++ raygame/Waypoint.cpp
@@ -0,0 +1,6 @@
+#include "Waypoint.h"
+
+Waypoint::Waypoint(float x, float y)
+{
+	m_position = Vector2(x, y);
+}
---
+++ raygame/Waypoint.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "raymath.h"
+
+static class Waypoint
+{
+private:
+	Vector2 m_position;
+
+public:
+	Waypoint(float x, float y);
+};
