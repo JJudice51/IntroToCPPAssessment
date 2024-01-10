a4b52caa9d28265a7cd40c65f7f764defa58dcff
Justin Judice <jjudice51@gmail.com>
2024-01-10 12:08:08 -0600

    Basics of the Spawner class
    need to fill this out more later but constructor added for now

--- raygame/Spawner.cpp
+++ raygame/Spawner.cpp
@@ -1,2 +1,20 @@
-include "Spawner.h"
+#include "Spawner.h"
+#include "Actor.h"
+
+Spawner::Spawner()
+{
+
+}
+
+Spawner::Spawner(float spawnX, float spawnY, const char* name)
+{
+
+}
+
+void::SpawnActor()
+{
+	Actor newGuy = new Actor()
+		//i want to make an actor have that actor enter the scene at the location of the spawner how the fuck do i do that... no idea where to start kinda freaking out. i have no idea
+		//how this works in c++
+}
 
--- raygame/Spawner.h
+++ raygame/Spawner.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "Actor.h"
-
+class Actor
 
 class Transform2D;
 
@@ -8,16 +8,26 @@ class Transform2D;
 class Spawner : public Actor
 {
 private:
+	Actor* m_actorsToAdd;
 	
-	Actor m_owner;
-
-
-
 
 public:
 
-	Spawner(Actor owner);
+	Spawner();
 
+	/// <summary>
+	/// A Spawner's purpose is to create an instance of a pareticular actor and add it to the Scene.
+	/// </summary>
+	/// <param name="x">position on the x axis that the spawner is located</param>
+	/// <param name="y">position on the y axis that the spawner is located</param>
+	/// <param name="name">name of the spawner</param>
+	/// <param name=""></param>
+	Spawner(float x, float y, const char* name);
+
+	/// <summary>
+	/// This function creates an instance of the Actor in the parameters list in the scene.
+	/// </summary>
+	SpawnActor();
 
 
 
