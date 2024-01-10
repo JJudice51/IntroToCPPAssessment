467c6a30fddfd46e740c323a6349b01cd83115cf
Christian Deese <s238007@students.aie.edu>
2024-01-10 15:04:12 -0600

    added normalization of Projectile trajectories and added comments to Projectile.cpp.

--- raygame/Projectile.cpp
+++ raygame/Projectile.cpp
@@ -3,15 +3,21 @@
 
 Projectile::Projectile(char* name, char* spritePath, Actor* shooter, float despawnTime) : Actor::Actor(shooter->getTransform()->getWorldPosition().x, shooter->getTransform()->getWorldPosition().y, "Actor")
 {
+	//stores the name of the Projectile
 	m_name = name;
+	//stores a reference to the shooter of the Projectile
 	m_shooter = shooter;
 	
+	//stores the trajectory of the Projectile
 	m_trajectory = MathLibrary::Vector2(0, 0);
+	//keeps track of the point in time that the Projectile is supposed to despawn.
 	m_despawnTime = despawnTime;
+	//keeps track of the current point in time.
 	m_currentTime = 0;
 }
 
 void Projectile::updateTrajectory()
 {
-	m_trajectory = m_shooter->getTransform()->getForward();
+	//get and store the normalized direction that the shooter is facing in.
+	m_trajectory = (m_shooter->getTransform()->getForward()).getNormalized();
 }
