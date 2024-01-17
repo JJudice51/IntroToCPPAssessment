e398b80b14ce2d592458af36fc4dc49b66e2b132
Justin Judice <jjudice51@gmail.com>
2024-01-17 15:04:31 -0600

    Created the ProjectileWeaponComponent which is derived from WeaponComponent

---
+++ raygame/ProjectileWeaponComponent.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "WeaponComponent.h"
+#include "Projectile.h"
+#include "Spawner.h"
+#include "Character.h"
+class ProjectileWeaponComponent :public WeaponComponent
+{
+private:
+
+
+
+public:
+
+
+
+
+};
+
