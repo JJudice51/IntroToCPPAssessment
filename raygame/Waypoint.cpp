ed910e591de559210e6e285328c95be945689be0
Christian Deese <s238007@students.aie.edu>
2024-01-17 13:57:09 -0600

    added waypoint array to keep track of waypoints in waypoints
    this array and its functions are static

--- raygame/Waypoint.cpp
+++ raygame/Waypoint.cpp
@@ -1,5 +1,8 @@
 #include "Waypoint.h"
 
+//initalizes m_waypointCount only once in this space.
+int Waypoint::m_waypointCount = 0;
+
 Waypoint::Waypoint(float x, float y)
 {
 	//stores the position given to this Waypoint upon construction.
@@ -11,4 +14,50 @@ MathLibrary::Vector2 Waypoint::getPosition()
 	//returns the position of this Waypoint.
 	return m_position;
 }
+
+Waypoint** Waypoint::getWaypoints()
+{
+	//checks if there are waypoints.
+	if (m_waypointCount > 0)
+	{
+		//returns a reference to the waypoint array.
+		return m_waypoints;
+	}
+
+	//if there are no waypoints in m_waypoints, return nullptr.
+	return nullptr;
+}
+
+void Waypoint::addWaypoint(Waypoint* waypoint)
+{
+	//if waypoint count is not 0 or null
+	if (m_waypointCount > 0)
+	{
+		//create a tempArray 1 bigger than the last.
+		Waypoint** tempArray = new Waypoint * [m_waypointCount + 1];
+
+		//iterate through the old array
+		for (int i = 0; i < m_waypointCount; i++)
+		{
+			//every iteration put a new item into the new array from the old one.
+			tempArray[i] = m_waypoints[i];
+		}
+		//enter the new waypoint to add to the new array
+		tempArray[m_waypointCount] = waypoint;
+	}
+	else
+	{
+		//else if waypoints is empty, create a new array with waypoint in it to be referenced.
+		m_waypoints = new Waypoint * [1] { waypoint };
+	}
+	//increase the waypoint count because we added one.
+	m_waypointCount++;
+}
+
+int Waypoint::getLength()
+{
+	//returns a copy of the variable that stores the amount of waypoints inside of the static waypoint array.
+	return m_waypointCount;
+}
+
 
--- raygame/Waypoint.h
+++ raygame/Waypoint.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Vector2.h"
+#include "ActorArray.h"
 
 static class Waypoint
 {
@@ -8,7 +9,15 @@ private:
 	/// stores the position of this Waypoint
 	/// </summary>
 	MathLibrary::Vector2 m_position;
-
+	/// <summary>
+	/// references the array that references the waypoints that have been created.
+	/// </summary>
+	static Waypoint** m_waypoints;
+	/// <summary>
+	/// the amount of waypoints that are stored inside of the static waypoint array.
+	/// defined and initialized at the top of the .cpp.
+	/// </summary>
+	static int m_waypointCount;
 public:
 	/// <summary>
 	/// constructs a Waypoint at a designated position
@@ -16,9 +25,25 @@ public:
 	/// <param name="x">position of the Waypoint on the x axis</param>
 	/// <param name="y">position of the Waypoint on the y axis</param>
 	Waypoint(float x, float y);
+	~Waypoint();
 	/// <summary>
 	/// gets the position of this Waypoint
 	/// </summary>
 	/// <returns>a Vector2 representing this Waypoint's position</returns>
 	MathLibrary::Vector2 getPosition();
+	/// <summary>
+	/// gets a reference the the static waypoint array.
+	/// </summary>
+	/// <returns>a reference to m_waypoints</returns>
+	static Waypoint** getWaypoints();
+	/// <summary>
+	/// adds a waypoint to the static waypoint array.
+	/// </summary>
+	/// <param name="waypoint">the waypoint to add to the static waypoint array</param>
+	static void addWaypoint(Waypoint* waypoint);
+	/// <summary>
+	/// gets the length of the static waypoint array
+	/// </summary>
+	/// <returns>the length of the static array inside of the Waypoint class.</returns>
+	static int getLength();
 };
