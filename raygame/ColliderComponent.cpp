0ab52fc91fe3f11d4654d533a23693f090b632c7
Justin Judice <jjudice51@gmail.com>
2024-01-17 11:28:27 -0600

    Not sure why this updated in the Spawner branch
    going to push this and merge it to my ColliderComponent so that this is in that branch as well

---
+++ raygame/ColliderComponent.cpp
@@ -0,0 +1,5 @@
+#include "ColliderComponent.					
+ColliderComponent::ColliderComponent(float radius, Actor* owner)
+{
+	m_owner = owner;
+	m_rad
---
+++ raygame/ColliderComponent.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "Component.h"
+#include "Collider.h"
+#include "CircleCollider.h"
+#include "AABBCollider.h"
+
+
+
+class ColliderComponent :
+    public Component
+{
+
+private:
+    Actor* m_owner;
+    float m_radius;
+    
+
+
+public:
+   
+    /// <summary>
+    /// Collider Component stores the collider data for any Actor that has an collider attached to them.
+    /// </summary>
+    /// <param name="owner">owner of the collider that this component is tracking for a given actor</param>
+    ColliderComponent(Actor* owner);
+
+    ColliderComponent(float radius, Actor* owner);
+    
+
+
+
+
+};
+
