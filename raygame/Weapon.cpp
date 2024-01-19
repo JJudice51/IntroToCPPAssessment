3c5b393efdeef55b3eee37a060829ea9b12bc9c1
Christian Deese <s238007@students.aie.edu>
2024-01-19 13:57:43 -0600

    ran into an error while tweaking Weapons.

--- raygame/Weapon.cpp
+++ raygame/Weapon.cpp
@@ -1,9 +1,10 @@
 #include "Weapon.h"
 
-Weapon::Weapon(Actor* owner, MathLibrary::Vector2 facing, MathLibrary::Vector2 position, float damage, char* name)
+Weapon::Weapon(Actor* owner, MathLibrary::Vector2 facing, MathLibrary::Vector2 position, float damage, char* name) : Actor::Actor(owner, "Weapon")
 {
-	m_owner = owner;
+	//stores the direction that this is facing.
 	m_facing = facing;
+	//st
 	m_position = position;
 	m_damage = damage;
 	m_name = name;
