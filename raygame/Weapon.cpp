082f74ed0aeaf0ccc88cbde7be929e0d685a53fe
Christian Deese <s238007@students.aie.edu>
2024-01-19 15:27:53 -0600

    fixed an incorrect Actor constructor syntax error

--- raygame/Weapon.cpp
+++ raygame/Weapon.cpp
@@ -1,6 +1,6 @@
 #include "Weapon.h"
 
-Weapon::Weapon(Actor* owner, MathLibrary::Vector2 facing, MathLibrary::Vector2 position, float damage, char* name) : Actor::Actor(owner, "Weapon")
+Weapon::Weapon(Actor* owner, MathLibrary::Vector2 facing, MathLibrary::Vector2 position, float damage, char* name) : Actor::Actor(20, 20, "Weapon")
 {
 	//stores the direction that this is facing.
 	m_facing = facing;
