51bb08875ff8efe7c5d254b1bfc12864871551d3
Christian Deese <s238007@students.aie.edu>
2024-01-19 12:27:41 -0600

    finished designing WeaponComponent that stores Weapons.

--- raygame/WeaponComponent.cpp
+++ raygame/WeaponComponent.cpp
@@ -1,6 +1,32 @@
 #include "WeaponComponent.h"
+#include "Weapon.h"
 
-WeaponComponent::WeaponComponent(Actor* owner)
+WeaponComponent::WeaponComponent(Actor* owner) : Component::Component(owner, "Weapon")
 {
-	owner = m_owner;
+	//stores a reference to the owner of this Component.
+	m_owner = owner;
+	//gives a default value to m_equippedWeapon
+	m_equippedWeapon = nullptr;
 }
+
+WeaponComponent::WeaponComponent(Actor* owner, Weapon* weapon) :  Component::Component(owner, "Weapon")
+{
+	//stores a reference to the owner of this component
+	m_owner = owner;
+	//stores a reference to the weapon that the Actor, that owns this component, has equipped.
+	m_equippedWeapon = weapon;
+}
+
+Weapon* WeaponComponent::getWeapon()
+{
+	//returns the Weapon that the Actor, that this is attached to, has equipped.
+	return m_equippedWeapon;
+}
+
+void WeaponComponent::setWeapon(Weapon* weapon)
+{
+	//store the reference location in weapon to m_equippedWeapon
+	m_equippedWeapon = weapon;
+}
+
+
--- raygame/WeaponComponent.h
+++ raygame/WeaponComponent.h
@@ -1,35 +1,41 @@
 #pragma once
 #include "Component.h"
 #include "Transform2D.h"
-#include "CircleCollider.h"
-#include "AABBCollider.h"
+
+//forward declare weapon to avoid circular dependancy.
+class Weapon;
 
 class WeaponComponent : public Component
 {
-
 private:
-	//the Actor who owns a given WeaponComponent.
+	//stores a reference to the Actor who owns a given WeaponComponent.
 	Actor* m_owner;
-
+	//stores a reference to the Weapon that this Actor 
+	Weapon* m_equippedWeapon;
 
 public:
-
 	/// <summary>
-	/// Constructs a WeaponComponent for a given Actor. A WeaponCopmpponent should allow an Actor to do damage to another Actor of a different faction in the game.
+	/// Constructs a WeaponComponent for a given Actor. A WeaponComponent should allow an Actor to do damage to another Actor, of a different faction, in the game.
 	/// </summary>
-	/// <param name="owner">The actor who owns this weapon component</param>
+	/// <param name="owner">The Actor who owns this Weapon Component</param>
 	WeaponComponent(Actor* owner);
 
+	/// <summary>
+	/// Constructs a WeaponComponent for a given Actor. A WeaponComponent should allow an Actor to do damage to another Actor, of a different faction, in the game.
+	/// </summary>
+	/// <param name="owner">The actor who owns this weapon component</param>
+	/// <param name="weapon">The weapon that will be equipped to the Actor, who owns this component.</param>
+	WeaponComponent(Actor* owner, Weapon* weapon);
 
+	/// <summary>
+	/// gets the equipped Weapon of the Actor that this is attached to.
+	/// </summary>
+	/// <returns>a reference to the Weapon that the Actor, this is attached to, has equipped.</returns>
+	Weapon* getWeapon();
 
-
-
-
-
-	//make it so that whoever has the weapon component can wield a weapon.
-
-
-
-
-
+	/// <summary>
+	/// stores a reference, to this Weapon, in WeaponComponent.
+	/// </summary>
+	/// <param name="weapon">the Weapon to store in WeaponComponent.</param>
+	void setWeapon(Weapon* weapon);
 };
