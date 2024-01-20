eab624dd4a402f8c43eb87c5fb9a72c184af5121
Justin Judice <jjudice51@gmail.com>
2024-01-19 20:37:50 -0600

    Childed Character wingMan to player1
    created a Character childed to the player character. If scaled down too small it will detect collision and delete childed wingMan character

--- raygame/SampleScene.cpp
+++ raygame/SampleScene.cpp
@@ -10,7 +10,17 @@ void SampleScene::start()
 	player1->getTransform()->setScale({ 50, 50 });
 	addActor(player1);
 
+
 	Character* enemy = new Character(300, 100, 20, "Images/Enemy.png", "Enemy");
 	enemy->getTransform()->setScale({ 50,50 });
 	addActor(enemy);
+	
+	Character* wingMan = new Character(2, 2,10, "Images/Player.png", "wingman");
+	addActor(wingMan);
+	wingMan->getTransform()->setScale({ 1,1 });
+	player1->getTransform()->addChild(wingMan->getTransform());
+
+	
+	
+
 }
