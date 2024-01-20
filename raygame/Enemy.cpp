a99bd8263f59f65794ec651b102472fc7a6321f8
Justin Judice <jjudice51@gmail.com>
2024-01-19 18:48:21 -0600

    With more deletions essentially creates enemy as another character
    had to remove several includes to allow for the enemy class to function without causing build errors.

--- raygame/Enemy.cpp
+++ raygame/Enemy.cpp
@@ -1,8 +1,7 @@
 #include "Enemy.h"
 #include "Transform2D.h"
-#include "Waypoint.h"
 #include "MoveComponent.h"
-#include "SpawnManager.h"
+
 
 Enemy::Enemy(float x, float y, float speed) : Character::Character(x, y, speed, "Enemy", "Enemy")
 {
--- raygame/Enemy.h
+++ raygame/Enemy.h
@@ -1,15 +1,12 @@
 #pragma once
 
 #include "Character.h"
-class Waypoint;
+
 
 class Enemy : public Character
 {
 private:
-	/// <summary>
-	/// stores a reference to the Waypoint the Enemy is currently targeting.
-	/// </summary>
-	Waypoint* m_currentPoint;
+	
 
 public:
 	/// <summary>
@@ -21,9 +18,7 @@ public:
 	/// <param name="speed">the speed that the Enemy will move.</param>
 	Enemy(float x, float y, float speed);
 
-	Actor* getNearestVisibleTarget();
 
-	Waypoint* findNewWaypoint();
 
 
 	/// <summary>
@@ -32,9 +27,5 @@ public:
 	/// <param name="deltaTime">the amount of time that has passed between the last frame and the current one.</param>
 	void update(float deltaTime) override;
 
-	/// <summary>
-	/// despawns this enemy with SpawnManager.
-	/// should be called once this Character is below 0.1f health.
-	/// </summary>
-	void end();
+	
 };
